5c197ee2319fd3805233c2bbcee17d4970c48bdd
Therizno <chief.20@icloud.com>
2019-06-22 01:37:05 -0400

    Debug and secret key default
    Changed settings for development branch

--- BPG_site/BPG_site/settings.py
+++ BPG_site/BPG_site/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'p7)1*6^5nm20)iufy#mnzc(1t_xr6w3q9!69%-#7fkvupcvud('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
